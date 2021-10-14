63f59053c153856dade6860da5985d67fd481699
SwordFish7249 <cem.gokdel@ktbmo.org>
2021-10-15 00:53:28 +0300

    Third Commit

---
+++ src/Components/EnjoyablePlaces.js
@@ -0,0 +1,8 @@
+
+function createEnjoyablePlace(Pname, price, hours, menu) {
+    const EPlace = {};
+    EPlace.Pname = Pname;
+    EPlace.price = price;
+    EPlace.hours = hours;
+    EPlace.menu = menu;
+}
